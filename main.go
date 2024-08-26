e8a282cb98614b1b34110208f0542093a43d3857
Paulus Donny Junianto <paulus.junianto@bytedance.com>
2024-08-26 21:51:27 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,14 +2,9 @@ package main
 
 import (
   "fmt"
-  "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello world!")
-}
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":443", nil)
+  fmt.Println("Hello")
 }
