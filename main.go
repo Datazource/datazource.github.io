5334d2db6b607574419abb231a5cf05b98a10e04
Paulus Donny Junianto <paulus.junianto@bytedance.com>
2024-08-26 21:27:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-    "fmt"      // formatting and printing values to the console.
-    "log"      // logging messages to the console.
-    "net/http" // Used for build HTTP servers and clients.
+  "fmt"
+  "net/http"
 )
 
-// Port we listen on.
-const portNum string = ":8080"
-
-// Handler functions.
-func Home(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Homepage")
-}
-
-func Info(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Info page")
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello world!")
 }
 
 func main() {
-    log.Println("Starting our simple http server.")
-
-    // Registering our handler functions, and creating paths.
-    http.HandleFunc("/", Home)
-    http.HandleFunc("/info", Info)
-
-    log.Println("Started on port", portNum)
-    fmt.Println("To close connection CTRL+C :-)")
-
-    // Spinning up the server.
-    err := http.ListenAndServe(portNum, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":80", nil)
 }
